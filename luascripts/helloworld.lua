e173642de06be93d26066cab475dc83f9088d805
Sander <97965051+GDS-Sander@users.noreply.github.com>
2022-09-11 17:55:49 +0200

    Added 2 scripts

---
+++ luascripts/helloworld.lua
@@ -0,0 +1,16 @@
+--
+--* @project: KZX Engine
+--* @author: Grand Dog Studio
+--
+
+local helloworld =
+{
+}
+
+function helloworld:OnStart()
+  
+  Debug.Log(tostring("Hello World :D"))
+
+end
+
+return helloworld
